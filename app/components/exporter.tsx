0c3b358cd1c7abb95c01e2ae32a22f140675abaf
king shaw <1343480152@qq.com>
2023-06-06 02:13:30 +0000

    commit

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,10 +437,8 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
-            <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
-            </div>
+            <div className={styles["main-title"]}>ChatGPT Web</div>
+            <div className={styles["sub-title"]}>https://www.shawking.live</div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
